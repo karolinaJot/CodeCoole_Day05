a179ffbcdd3740c66386e0a0aeef7f3d1830b632
karolinaJot <amicus.karo@gmail.com>
2023-05-10 13:31:13 +0200

    finished task 2

--- SQL_02_Repositories/PersonsRepository.cs
+++ SQL_02_Repositories/PersonsRepository.cs
@@ -27,7 +27,7 @@ namespace SQL_02_Repositories
 						{
 							Person person = new Person();
 							person.Id = reader.GetInt32(0);
-							person.FirstName = reader.GetString(1);	
+							person.FirstName = reader.GetString(1);
 							person.LastName = reader.GetString(2);
 
 							persons.Add(person);
@@ -35,7 +35,82 @@ namespace SQL_02_Repositories
 					}
 				}
 			}
-			return persons.ToArray();	
+			return persons.ToArray();
+		}
+		public void CreatePerson(Person person)
+		{
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+
+				using (SqlCommand command = new SqlCommand("INSERT INTO Persons (FirstName,LastName) VALUES(@FirstName, @LastName)", connection))
+				{
+					command.Parameters.AddWithValue("@FirstName", person.FirstName);
+					command.Parameters.AddWithValue("@LastName", person.LastName);
+					int rowsAffected = command.ExecuteNonQuery();
+					//Console.WriteLine($"{rowsAffected} rows affected.");
+				}
+
+
+
+
+
+
+			}
+
+		}
+
+		public void DeletePerson(int id)
+		{
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+				using (SqlCommand command = new SqlCommand("DELETE FROM Persons WHERE Id = @Id", connection))
+				{
+					command.Parameters.AddWithValue("@Id", id);
+					int rowsAffected = command.ExecuteNonQuery();
+					//Console.WriteLine($"{rowsAffected} rows affected.");
+				}
+			}
+		}
+
+		public void UpdatePerson(Person person)
+		{
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+				using (SqlCommand command = new SqlCommand("UPDATE Persons SET FirstName = @FirstName, LastName =@LastName WHERE Id = @Id", connection))
+				{
+					command.Parameters.AddWithValue("@Id", person.Id);
+					command.Parameters.AddWithValue("@FirstName", person.FirstName);
+					command.Parameters.AddWithValue("@LastName", person.LastName);
+					int rowsAffected = command.ExecuteNonQuery();
+					Console.WriteLine($"{rowsAffected} rows affected.");
+				}
+			}
+		}
+
+		public void TestingMethod(int a, int b, int c)
+		{
+
+		}
+		public void TestingMethod2(int[] arr)
+		{
+
+		}
+		// you can add much parameters as you want 
+		public void TestingMethod3(params int[] arr)
+		{
+
 		}
 	}
 }
+
+
--- SQL_02_Repositories/Program.cs
+++ SQL_02_Repositories/Program.cs
@@ -4,8 +4,19 @@ PersonsRepository personsRepository = new PersonsRepository();
 
 Person[] people=  personsRepository.GetPersons();
 
+Person newPerson = new Person()
+{
+	FirstName = "jan",
+	LastName = "kowalski"
+};
+
+personsRepository.CreatePerson(newPerson);
+personsRepository.DeletePerson(1);
+
 foreach (Person person in people) {
 	Console.WriteLine($"{person.FirstName}  {person.LastName}");
 }
 
+
+personsRepository.TestingMethod(1, 2, 3);
 
